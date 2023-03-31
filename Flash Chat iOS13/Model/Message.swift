f8a1423a9c0f58294ef29c62f35b0cfe47a1a76d
Mostafa <mostafa.omrane@proton.me>
2023-03-31 15:01:59 +0200

    Everything DONE

---
+++ Flash Chat iOS13/Model/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by VegaPunk on 28/03/2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+  let sender: String //email adress of the person
+  let body: String
+}
